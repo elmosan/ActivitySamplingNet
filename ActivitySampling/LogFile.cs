e4802ca186b990b7a644bd2e90b87e9d61f5eff2
elmosan <33811806+elmosan@users.noreply.github.com>
2018-02-05 13:25:52 +0100

    copy paste

--- ActivitySampling/LogFile.cs
+++ ActivitySampling/LogFile.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace ActivitySampling
 {
@@ -10,12 +10,33 @@ namespace ActivitySampling
     {
         private string _filename = @"ActivitySampling.log";
 
+        public void Init()
+        {
+            var file = new FileInfo(_filename);
+
+            if (file.Exists)
+                file.CopyTo($"{_filename.Replace(file.Extension, "")}-{DateTime.Now:yyyyMMddHHmmss}.log.bkp");
+        }
+
         public void Write(string message)
         {
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(_filename, true))
+            using (StreamWriter file = new StreamWriter(_filename, true))
             {
                 file.WriteLine(message);
             }
+        }
+
+        public IEnumerable<string> Load()
+        {
+            var logs = "";
+            var list = new List<string>();
+
+            foreach (var line in File.ReadLines(_filename).Reverse())
+            {
+                list.Add(line);
+            }
+
+            return list;
         }
     }
 }
--- ActivitySampling/MainWindow.xaml.cs
+++ ActivitySampling/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Input;
+using Clipboard = System.Windows.Clipboard;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 
 
 namespace ActivitySampling
@@ -21,6 +24,10 @@ namespace ActivitySampling
             InitializeComponent();
             _notifier = new NotifyTimer(15 * 60 * 1000);
             _logFile = new LogFile();
+            _logFile.Init();
+
+            ReadLog();
+
             //_notifier.OnBallonClicked += NotifierTimerOnBallonClicked;
             _notifier.OnTimerElapsed += NotifierOnOnTimerElapsed;
 
@@ -34,6 +41,8 @@ namespace ActivitySampling
             };
             ballon.BalloonTipClicked += BallonOnBalloonTipClicked;
             //ballon.BalloonTipClosed += BallonNotifierOnBalloonTipClosed;
+            LstOutput.MouseDoubleClick += LstOutputOnMouseDoubleClick;
+            LstOutput.KeyUp += LstOutputOnKeyUp;
         }
 
         private void MainWindow_OnActivated(object sender, EventArgs e)
@@ -50,13 +59,23 @@ namespace ActivitySampling
             WriteLog();
         }
 
+        private void ReadLog()
+        {
+            var logs = _logFile.Load();
+
+            foreach (var item in logs)
+            {
+                LstOutput.Items.Add(item);
+            }
+        }
+
         private void WriteLog()
         {
             var msg = $"{DateTime.Now}: {TxtActivity.Text}";
             LstOutput.Items.Insert(0, msg);
             _logFile.Write(msg);
         }
-
+        
         private void NotifierOnOnTimerElapsed(object sender, EventArgs eventArgs)
         {
             ballon.ShowBalloonTip(3000, "Current Activity", "What are you doing?", ToolTipIcon.Info);
@@ -70,6 +89,25 @@ namespace ActivitySampling
         private void TxtIntervall_OnLostFocus(object sender, RoutedEventArgs e)
         {
             _notifier.SetIntervall(int.Parse(TxtIntervall.Text));
+        }
+
+        private void LstOutputOnMouseDoubleClick(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            var text = LstOutput.SelectedItem.ToString().Substring(21);
+            TxtActivity.Text = text;
+        }
+
+        private void LstOutputOnKeyUp(object sender, KeyEventArgs keyEventArgs)
+        {
+            var key = keyEventArgs.Key;
+            var text = LstOutput.SelectedItem.ToString().Substring(21);
+
+            if (key == Key.C && keyEventArgs.KeyboardDevice.Modifiers == ModifierKeys.Control)
+            {
+                Clipboard.SetText(text);
+                TxtActivity.Text = text;
+            }
+
         }
     }
 }
