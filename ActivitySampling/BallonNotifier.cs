e9ab3f3ed3120c999c341c45279bb2d24a844202
elmosan <33811806+elmosan@users.noreply.github.com>
2018-02-05 07:32:27 +0100

    improvements

---
+++ ActivitySampling/BallonNotifier.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ToastNotifications;
+using ToastNotifications.Core;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Position;
+
+namespace ActivitySampling
+{
+    public class BallonNotifier : IDisposable
+    {
+        //private Notifier _toastNotifier;
+
+        public void Notify()
+        {
+            //_toastNotifier = new Notifier(cfg =>
+            //{
+            //    cfg.PositionProvider = new WindowPositionProvider(
+            //        parentWindow: Application.Current.MainWindow,
+            //        corner: Corner.TopRight,
+            //        offsetX: 10,
+            //        offsetY: 10);
+
+            //    cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+            //        notificationLifetime: TimeSpan.FromSeconds(3),
+            //        maximumNotificationCount: MaximumNotificationCount.FromCount(5));
+
+            //    cfg.Dispatcher = Application.Current.Dispatcher;
+            //});
+
+            //var title = "ActivitySampling";
+            //var message = "Test Messsage";
+
+            //var options = new MessageOptions
+            //{
+            //    NotificationClickAction = n =>
+            //    {
+            //        n.Close();
+            //        _toastNotifier.ShowSuccess("clicked!");
+            //    }
+            //};
+
+            //_toastNotifier.ShowInformation(message, options);
+            //_toastNotifier.ShowSuccess(message);
+            //_toastNotifier.ShowWarning(message);
+            //_toastNotifier.ShowError(message);
+        }
+
+        public void Notify(string title, string message)
+        {
+            Thread.Sleep(4000);
+
+            NotifyIcon ballon = new NotifyIcon
+            {
+                Text = message,
+                BalloonTipText = message,
+                BalloonTipTitle = title,
+                Visible = true,
+                Icon = new Icon("ActivityMonitor.ico")
+            };
+            ballon.BalloonTipClicked += BallonNotifierOnBalloonTipClicked;
+            //ballon.BalloonTipClosed += BallonNotifierOnBalloonTipClosed;
+            ballon.ShowBalloonTip(3000, title, message, ToolTipIcon.Info);
+        }
+
+        public event EventHandler OnBalloonTipClicked;
+
+        public void Dispose()
+        {
+            //_toastNotifier.Dispose();
+            //_ballon.BalloonTipClicked -= BallonNotifierOnBalloonTipClicked;
+        }
+
+        private void BallonNotifierOnBalloonTipClicked(object o, EventArgs eventArgs)
+        {
+            OnBalloonTipClicked?.Invoke(this, null);
+        }
+
+        private void BallonNotifierOnBalloonTipClosed(object o, EventArgs eventArgs)
+        {
+            //_ballon.BalloonTipClicked -= BallonNotifierOnBalloonTipClicked;
+        }
+    }
+}
---
+++ ActivitySampling/NotifyTimer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ActivitySampling
+{
+    public class NotifyTimer : IDisposable
+    {
+        private readonly Timer _timer;
+        //private readonly BallonNotifier _ballonNotifier;
+
+        public NotifyTimer(int intervall)
+        {
+            _timer = new Timer();
+            //_ballonNotifier = new BallonNotifier();
+
+            _timer.Interval = intervall;
+            _timer.Elapsed += TimerOnElapsed;
+            //_ballonNotifier.OnBalloonTipClicked += BallonNotifierOnOnBalloonTipClicked;
+            _timer.Start();
+        }
+
+        //public event EventHandler OnBallonClicked;
+        public event EventHandler OnTimerElapsed;
+
+        public void Dispose()
+        {
+            _timer.Stop();
+            _timer.Elapsed -= TimerOnElapsed;
+            //_ballonNotifier.OnBalloonTipClicked -= BallonNotifierOnOnBalloonTipClicked;
+        }
+
+        public void SetIntervall(int intervall)
+        {
+            _timer.Interval = intervall;
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            //_timer.Stop();
+            //_ballonNotifier.Notify("Current Activity", "What are you doing?");
+
+            OnTimerElapsed?.Invoke(this, null);
+        }
+
+        //private void BallonNotifierOnOnBalloonTipClicked(object o, EventArgs eventArgs)
+        //{
+        //    OnBallonClicked?.Invoke(this, null);
+        //}
+    }
+}
